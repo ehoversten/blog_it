9daae10186755d3d7d631a2d546575b08270e002
ehoversten <ehoversten@gmail.com>
2019-03-15 13:01:42 -0400

    added base testing route

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-mongoose.connect();
+// mongoose.connect();
 
 const app = express();
+
+// ROUTES
+app.get('/', (req, res) => {
+    res.send('Index Landing Page');
+});
 
 const PORT = process.env.PORT || 5000;
 
